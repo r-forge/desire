25ff5f731f8864fe59721742009577535cc4e776
olafm (Olaf Mersmann) <olafm@kimberly.tako.de>
2008-08-19 18:46:00 +0000

    * Final version of useR talk.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/desire@86 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- papers/useR_2008/useR_2008-presentation.R
+++ papers/useR_2008/useR_2008-presentation.R
@@ -8,22 +8,23 @@ responseName <- function(di) {
   }
 }
 
-##########################################################################################
+mkPanel <- function(x, y, other.panel) {
+  panel <- function(...) {
+    other.panel(...)
+    panel.abline(h=y, v=x, col="red", lty=2)
+  }
+  return(panel)
+}
+
+colramp <- grey(1:200/201)
+
+###############################################################################
 ## Demo starts here:
 require(desiRe)
+require(lattice)
 
-schoko <- read.table("useR_2008-presentation.txt", sep=";", dec=",", head=TRUE)
-schoko$RT <- NULL
-schoko$srun <- NULL
-
-summary(schoko)
-
-## Use linear models as predictor:
-m.E     <- lm(E ~ rt + as + I(rt^2) + I(as^2) + rt:as, schoko)
-m.d90   <- lm(d90 ~ rt + as + I(rt^2) + I(as^2) + rt:as, schoko)
-m.Fe    <- lm(Fe ~ rt + as + I(rt^2) + I(as^2) + rt:as, schoko)
-m.etaCa <- lm(etaCa ~ rt + as + I(rt^2) + I(as^2) + rt:as, schoko)
-m.tauCa <- lm(tauCa ~ rt + as + I(rt^2) + I(as^2) + rt:as, schoko)
+data(Chocolate)
+summary(Chocolate)
 
 ## Construct desirabilities
 d.rt    <- derringerSuich(c(-Inf, 30, 45, 1, 1))
@@ -33,17 +34,32 @@ d.Fe    <- derringerSuich(c(-Inf, 20, 30, 1, 1))
 d.etaCa <- derringerSuich(c(1, 1.5, 2, 1, 1))
 d.tauCa <- derringerSuich(c(5, 8, 10, 1, 1))
 
-d.Fe2 <- d.Fe
-class(d.Fe2) <- c("dsA1", class(d.Fe2))
+## Use linear models as predictor:
+m.E     <- lm(E ~ rt + as + I(rt^2) + I(as^2) + rt:as, Chocolate)
+m.d90   <- lm(d90 ~ rt + as + I(rt^2) + I(as^2) + rt:as, Chocolate)
+m.Fe    <- lm(Fe ~ rt + as + I(rt^2) + I(as^2) + rt:as, Chocolate)
+m.etaCa <- lm(etaCa ~ rt + as + I(rt^2) + I(as^2) + rt:as, Chocolate)
+m.tauCa <- lm(tauCa ~ rt + as + I(rt^2) + I(as^2) + rt:as, Chocolate)
 
 ## Plot of desirabilities
 opar <- par(mfrow=c(3, 2))
-plot(d.rt, main="Runtime")
-plot(d.E, main="Energy")
-plot(d.d90, main="Particle size")
-plot(d.Fe, main="Iron content")
-plot(d.etaCa, main="Plastic viscosity")
-plot(d.tauCa, main="yield value")
+plot(d.rt, main="Runtime", xlim=c(25, 50))
+curve(realisticDF(d.rt)(x, sd=1), n=203, add=TRUE, col="red")
+
+plot(d.E, main="Energy", xlim=c(2, 5))
+curve(realisticDF(d.E)(x, sd=.145), n=203, add=TRUE, col="red")
+
+plot(d.d90, main="Particle size", xlim=c(15, 28))
+curve(realisticDF(d.d90)(x, sd=1.73), n=203, add=TRUE, col="red")
+
+plot(d.Fe, main="Iron content", xlim=c(15, 35))
+curve(realisticDF(d.Fe)(x, sd=.71), n=203, add=TRUE, col="red")
+
+plot(d.etaCa, main="Plastic viscosity", xlim=c(-1, 4))
+curve(realisticDF(d.etaCa)(x, sd=.81), n=203, add=TRUE, col="red")
+
+plot(d.tauCa, main="yield value", xlim=c(3, 12))
+curve(realisticDF(d.tauCa)(x, sd=.72), n=203, add=TRUE, col="red")
 par(opar)
 
 ## Goal:
@@ -58,6 +74,7 @@ fnrt <- function(x) {
   }
 }
 
+## Idealistic Desirabilities:
 cd.rt     <- compositeDF(fnrt, d.rt)
 cd.E      <- compositeDF(m.E, d.E)
 cd.d90    <- compositeDF(m.d90, d.d90)
@@ -65,51 +82,38 @@ cd.Fe     <- compositeDF(m.Fe, d.Fe)
 cd.etaCa  <- compositeDF(m.etaCa, d.etaCa)
 cd.tauCa  <- compositeDF(m.tauCa, d.tauCa)
 
+## Realistic Desirabilities:
+cd.E      <- compositeDF(m.E, realisticDF(d.E))
+cd.d90    <- compositeDF(m.d90, realisticDF(d.d90))
+cd.Fe     <- compositeDF(m.Fe, realisticDF(d.Fe))
+cd.etaCa  <- compositeDF(m.etaCa, realisticDF(d.etaCa))
+cd.tauCa  <- compositeDF(m.tauCa, realisticDF(d.tauCa))
+
 ## Combine desirabilities using geometric DI:
 di <- geometricDI(cd.rt, cd.E, cd.d90, cd.Fe, cd.etaCa, cd.tauCa,
                   weights=c(10, .1, 5, 1, 1, 1))
 
 ## Optimize!
-of <- optim(c(40, 70), di, control=list(fnscale=-1))
+opt <- optim(c(40, 70), di, control=list(fnscale=-1))
+opt <- optim(c(40, 70), di, method="BFGS", control=list(fnscale=-1))
+
+opt$par
 
 ## Visualize DI:
-rt <- seq(20, 45, length.out=201)
-as <- seq(5, 90, length.out=201)
-z <- outer(rt, as, function(x,y) di(data.frame(rt=x, as=y)))
-image(rt, as, z, col=grey(seq(0, 1, length.out=200)))
-abline(v=of$par[1], h=of$par[2], col="red", lty=2)
+n <- 101
+rt <- seq(20, 45, length.out=n)
+as <- seq(5, 90, length.out=n)
+sq <- data.frame(rt=rep(rt, each=n), as=rep(as, times=n))
 
-## Individual desirabilities:
-opar <- par(mfrow=c(3, 2))
+z <- di(sq)
+panel <- mkPanel(opt$par[1], opt$par[2], panel.levelplot)
+levelplot(z ~ rt * as, sq, panel=panel, cuts=50, col.regions=colramp)
+
+## IndividUalir desirabilities:
+res <- NULL
 for (dfn in c(cd.rt, cd.E, cd.d90, cd.Fe, cd.etaCa, cd.tauCa)) {
-  z <- outer(rt, as, function(x, y) dfn(data.frame(rt=x, as=y)))
-  image(rt, as, z, col=grey(seq(0, 1, length.out=200)), main=responseName(dfn))
-  abline(v=of$par[1], h=of$par[2], col="red", lty=2)
+  z <- dfn(sq)
+  res <- rbind(res, data.frame(fn=responseName(dfn), rt=sq$rt, as=sq$as, di=z))
 }
-par(opar)
-
-##########################################################################################
-## Realistic desirabilities:
-rcd.E      <- compositeDF(m.E, realisticDF(d.E))
-rcd.d90    <- compositeDF(m.d90, realisticDF(d.d90))
-rcd.Fe     <- compositeDF(m.Fe, realisticDF(d.Fe))
-rcd.etaCa  <- compositeDF(m.etaCa, realisticDF(d.etaCa))
-rcd.tauCa  <- compositeDF(m.tauCa, realisticDF(d.tauCa))
-
-rdi <- geometricDI(cd.rt, rcd.E, rcd.d90, rcd.Fe, rcd.etaCa, rcd.tauCa,
-                   weights=c(10, .1, 5, 1, 1, 1))
-
-og <- optim(c(40, 70), rdi, control=list(fnscale=-1))
-dput(og, "og.txt")
-## og <- dget("og.txt")
-
-## Deterministic/Idealized desirability of solution is 0!
-di(og$par)
-
-rz <- outer(rt, as, function(x,y) rdi(data.frame(rt=x, as=y)))
-dput(rz, "rz.txt")
-## rz <- dget("rz.txt")
-
-image(rt, as, rz, col=grey(seq(0, 1, length.out=200)))
-abline(v=og$par[1], h=og$par[2], col="red", lty=2)
-abline(v=of$par[1], h=of$par[2], col="blue", lty=2)
+levelplot(di ~ rt + as | fn, res,
+          panel=panel, cuts=50, col.regions=colramp)
